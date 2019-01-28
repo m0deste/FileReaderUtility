80170fbb837f480f359270520cc871588c0623c4
Modeste KOUASSI <modeste090@gmail.com>
2019-01-28 16:08:11 +0000

    Version 1.0

---
+++ FileReaderUtility/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileReaderUtility.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FileReaderUtility/ReadFiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FileReaderUtility
+{
+    public class LoaderFile
+    {
+        public static String ReadTextFile(String filePath)
+        {
+            return "";
+        }
+
+        public static String ReadXMLFile(String filePath)
+        {
+            return "";
+        }
+
+        public static String ReadJSONFile(String filePath)
+        {
+            return "";
+        }
+    }
+}
