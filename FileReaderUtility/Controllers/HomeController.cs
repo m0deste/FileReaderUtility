d1395bcf546c70347021eab23894f6da8d871910
Modeste KOUASSI <modeste090@gmail.com>
2019-01-28 16:15:52 +0000

    Version 1.1

--- FileReaderUtility/Controllers/HomeController.cs
+++ FileReaderUtility/Controllers/HomeController.cs
@@ -5,39 +5,83 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FileReaderUtility.Models;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using FileReaderUtility;
+using Microsoft.AspNetCore.Hosting;
 
 namespace FileReaderUtility.Controllers
 {
     public class HomeController : Controller
     {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
-        public IActionResult Contact()
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
         {
-            ViewData["Message"] = "Your contact page.";
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+
+
+            if (file.Length > 0)
+            {
+
+                //Getting FileName
+                var fileName = Path.GetFileName(file.FileName);
+                var fileExtension = Path.GetExtension(fileName);
+
+                var filePath = Path.Combine(uploads, file.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+
+                Console.Out.WriteLine("EXTENSION DE FICHIER: "+ fileExtension);
+               
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                switch (fileExtension) {
+                    case ".txt":
+                        ViewBag.Message = ReadFiles.ReadTextFile(filePath);
+                        break;
+                    case ".xml":
+                        ViewBag.Message = ReadFiles.ReadXMLFile(filePath);
+                        break;
+                    case ".json":
+                        ViewBag.Message = ReadFiles.ReadJSONFile(filePath);
+                        break;
+                    default:
+                        ViewBag.Message = "Fichier illisible!";
+                        break;
+                }
+
+
+            }
+            
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+           //return Ok(new { count = files.Count, size, filePath });
 
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- FileReaderUtility/ReadFiles.cs
+++ FileReaderUtility/ReadFiles.cs
@@ -1,21 +1,57 @@
 ﻿using System;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.FileProviders;
+
 namespace FileReaderUtility
 {
-    public class LoaderFile
+    public class ReadFiles
     {
         public static String ReadTextFile(String filePath)
         {
-            return "";
+
+            try
+            {
+                Console.Out.WriteLine("PATH DU FICHIER: " + filePath);
+
+                string contents;
+                var fileStream = new FileStream(@filePath, FileMode.Open, FileAccess.Read);
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    contents = streamReader.ReadToEnd();
+
+                   
+                }
+
+                if ((System.IO.File.Exists(filePath)))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+
+                Console.Out.WriteLine("CONTENU DU FICHIER: " + contents);
+                return contents;
+
+            }
+            catch (IOException e) {
+
+                Console.Out.WriteLine("MESSAGE ERREUR: "+e.Message);
+                return null;
+            }
+
         }
 
         public static String ReadXMLFile(String filePath)
         {
-            return "";
+            return ReadTextFile(filePath);
         }
 
         public static String ReadJSONFile(String filePath)
         {
             return "";
         }
+
     }
 }
